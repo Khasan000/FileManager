53ad4b98085bf1aa097f9994e2dcccfb8d045ad3
Khasan000 <zkurycin@gmail.com>
2023-03-13 23:58:26 +0300

    TEST

---
+++ Task_Ekzamen/Ekzamen/Ekzamen/da.h
@@ -0,0 +1,44 @@
+#pragma once
+/*while (true)
+	{
+		int choise;
+		cout << "Choose func:" << endl;
+		for (int i = 1; i < SIZE; ++i)
+			cout <<i<<")" << Functions[i] << endl;
+		cout << endl;
+
+		cin >> choise;
+
+		if (choise > 7 || choise < 1)
+		{
+			cout << "Out Of Range!";
+			break;
+		}
+
+
+
+
+		switch (choise)
+		{
+		case 1:
+			file.CreateFile();
+			break;
+		case 2:
+			file.DeleteFile();
+			break;
+		case 3:
+			file.RenameFile();
+			break;
+		case 4:
+			file.MovingFile();
+			break;
+		case 5:
+			file.CopyFile();
+			break;
+		case 6:
+			file.CalculatingSize();
+			break;
+		default:
+			cout << "Incorrect choise!" << endl;
+		}
+	}*/
