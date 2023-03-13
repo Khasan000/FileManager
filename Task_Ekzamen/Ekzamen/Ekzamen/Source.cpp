00a7e5c337d38a52af113094d9daa41e1888989c
Khasan000 <zkurycin@gmail.com>
2023-03-14 00:00:11 +0300

    5

--- Task_Ekzamen/Ekzamen/Ekzamen/Source.cpp
+++ Task_Ekzamen/Ekzamen/Ekzamen/Source.cpp
@@ -11,62 +11,12 @@
 
 int main()
 {
-	FileManager file;
-	/*file.CreateFile();*/ // +папка +file +file.manual
-	/*file.DeleteFile();*/ // +file - papka
-	//file.RenameFile(); //+file +papka
-	//file.MovingFile(); //+file +papka +papka c filom
-	//file.CopyFile();
-	//file.CalculatingSize();
 	const int SIZE = 7;
 	string Functions[SIZE]{"","CreateFile","DeleteFile","RenameFile","MovingFile","CopyFile","CalculatingSize"};
 
-	file.MovingFile();
-	file.MovingFile();
+	
 
-	/*while (true)
-	{
-		int choise;
-		cout << "Choose func:" << endl;
-		for (int i = 1; i < SIZE; ++i)
-			cout <<i<<")" << Functions[i] << endl;
-		cout << endl;
-
-		cin >> choise;
-
-		if (choise > 7 || choise < 1)
-		{
-			cout << "Out Of Range!"; 
-			break;
-		}
-
-		
-
-
-		switch (choise)
-		{
-		case 1:
-			file.CreateFile();
-			break;
-		case 2:
-			file.DeleteFile();
-			break;
-		case 3:
-			file.RenameFile();
-			break;
-		case 4:
-			file.MovingFile();
-			break;
-		case 5:
-			file.CopyFile();
-			break;
-		case 6:
-			file.CalculatingSize();
-			break;
-		default:
-			cout << "Incorrect choise!" << endl;
-		}
-	}*/
+	
 	
 	
 }
